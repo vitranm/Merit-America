df62d8cb2982912e0b5ad92f6780bb201f8dd9ed
Vi Tran <50426800+vitranm@users.noreply.github.com>
2019-08-17 15:19:00 -0500

    Add files via upload

---
+++ Week 14/Vi_Tran_GettingFamiliarWithRouting/GettingFamiliarWithRouting/src/main/java/com/vitran/GettingFamiliarWithRouting/GettingFamiliarWithRoutingApplication.java
@@ -0,0 +1,14 @@
+package com.vitran.GettingFamiliarWithRouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingFamiliarWithRoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingFamiliarWithRoutingApplication.class, args);
+	}
+}
---
+++ Week 14/Vi_Tran_GettingFamiliarWithRouting/GettingFamiliarWithRouting/src/test/java/com/vitran/GettingFamiliarWithRouting/GettingFamiliarWithRoutingApplicationTests.java
@@ -0,0 +1,20 @@
+package com.vitran.GettingFamiliarWithRouting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GettingFamiliarWithRoutingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
